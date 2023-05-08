0b0fb63abc2fc29dac01e9756b99654ab036b01c
Victor Cabral <victor_cabral_o@hotmail.com>
2023-05-08 19:25:15 -0300

    fix: sale routes

--- back-end/src/routes/saleRoutes.js
+++ back-end/src/routes/saleRoutes.js
@@ -1,10 +1,9 @@
 const { Router } = require('express');
 const saleController = require('../controllers/saleController');
-const validateToken = require('../middlewares/validateToken');
 
 const routes = Router();
 
-routes.post('/', validateToken, saleController.createSale);
+routes.post('/', saleController.createSale);
 routes.get('/', saleController.getAllSales);
 routes.put('/orders', saleController.updateSale);
 
