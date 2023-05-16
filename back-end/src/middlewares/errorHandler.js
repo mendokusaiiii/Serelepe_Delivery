ae6becb052ff74010127c15785f2dbb9aa882969
Victor Cabral <victor_cabral_o@hotmail.com>
2023-05-16 10:59:22 -0300

    feat: middleware for validate informations

--- back-end/src/middlewares/errorHandler.js
+++ back-end/src/middlewares/errorHandler.js
@@ -1,6 +1,11 @@
 const errors = [
+  { status: 400, message: 'Invalid empty fields' },
+  { status: 400, message: 'Invalid email' },
+  { status: 400, message: 'Username must have at least 12 characters' },
+  { status: 400, message: 'Password must have at least 6 characters' },
   { status: 401, message: 'Incorrect password' },
   { status: 401, message: 'Expired or invalid token' },
+  { status: 401, message: 'Unauthorized' },
   { status: 404, message: 'User not found' },
   { status: 404, message: 'Token not found' },
   { status: 409, message: 'User already exist' },
